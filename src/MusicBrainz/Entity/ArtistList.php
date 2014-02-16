b0493e056bf0d0b6074027dd4ad357e545ff64bb
David White <david@monkeyphp.com>
2014-02-16 18:48:02 +0000

    Created ArtistList

---
+++ src/MusicBrainz/Entity/ArtistList.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * ArtistList.php
+ *
+ * @category   MusicBrainz
+ * @package    MusicBrainz
+ * @subpackage MusicBrainz\Entity
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
+ *
+ * Copyright (C) David White
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace MusicBrainz\Entity;
+
+use Iterator;
+use Traversable;
+
+/**
+ * ArtistList
+ *
+ * @category   MusicBrainz
+ * @package    MusicBrainz
+ * @subpackage MusicBrainz\Entity
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
+ */
+class ArtistList implements Iterator
+{
+
+    /**
+     * Array of instances of Artist
+     *
+     * @var array
+     */
+    protected $artists = array();
+
+    /**
+     * Current count
+     *
+     * @var int
+     */
+    protected $count;
+
+    /**
+     * Current offset
+     *
+     * @var int
+     */
+    protected $offset;
+
+    /**
+     * Iterator position
+     *
+     * @var int
+     */
+    protected $position = 0;
+
+    /**
+     * Set the Artists
+     *
+     * @param array|Traversable $artists
+     *
+     * @return ArtistList
+     */
+    public function setArtists($artists = array())
+    {
+        if (is_array($artists) || ($artists instanceof \Traversable)) {
+            foreach ($artists as $artist) {
+                if ($artist instanceof Artist) {
+                    $this->addArtist($artist);
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Add an Artist to the ArtistList
+     *
+     * @param Artist $artist
+     *
+     * @return ArtistList
+     */
+    public function addArtist(Artist $artist)
+    {
+        if (! in_array($artist, $this->artists, true)) {
+            $this->artists[] = $artist;
+        }
+        return $this;
+    }
+
+    /**
+     * Return the Artists
+     *
+     * @return array
+     */
+    public function getArtists()
+    {
+        return $this->artists;
+    }
+
+    /**
+     * Set the count for the total number of matched records
+     *
+     * @param int $count
+     *
+     * @return ArtistSearch
+     */
+    public function setCount($count = 0)
+    {
+        $this->count = $count;
+        return $this;
+    }
+
+    /**
+     * Set the current offset (for paging)
+     *
+     * @param int $offset
+     * @return ArtistSearch
+     */
+    public function setOffset($offset = 0)
+    {
+        $this->offset = $offset;
+        return $this;
+    }
+
+    /**
+     * Return the current count
+     *
+     * @return int
+     */
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    /**
+     * Return the curent offset
+     *
+     * @return int
+     */
+    public function getOffset()
+    {
+        return $this->offset;
+    }
+
+    /**
+     * Iterator
+     * 
+     * @return Artist
+     */
+    public function current()
+    {
+        return $this->artists[$this->position];
+    }
+
+    /**
+     * Iterator
+     *
+     * @return int
+     */
+    public function key()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Iterator
+     *
+     * @return void
+     */
+    public function next()
+    {
+        ++$this->position;
+    }
+
+    /**
+     * Iterator
+     *
+     * @return void
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+    /**
+     * Iterator
+     *
+     * @return boolean
+     */
+    public function valid()
+    {
+        return isset($this->artists[$this->position]);
+    }
+}
---
+++ tests/unit/MusicBrainzTest/Entity/ArtistListTest.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * ArtistListTest.php
+ *
+ * @category   MusicBrainzTest
+ * @package    MusicBrainzTest
+ * @subpackage MusicBrainzTest\Entity
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
+ *
+ * Copyright (C) David White
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace MusicBrainzTest\Entity;
+
+use MusicBrainz\Entity\Artist;
+use MusicBrainz\Entity\ArtistList;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * ArtistListTest
+ *
+ * @category   MusicBrainzTest
+ * @package    MusicBrainzTest
+ * @subpackage MusicBrainzTest\Entity
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
+ */
+class ArtistListTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that we can set the artists property
+     *
+     * @covers \MusicBrainz\Entity\ArtistList::setArtists
+     */
+    public function testSetArtists()
+    {
+        $artists = array(
+            new Artist()
+        );
+        $artistList = new ArtistList();
+
+        $this->assertEmpty($artistList->getArtists());
+        $this->assertSame($artistList, $artistList->setArtists($artists));
+        $this->assertCount(1, $artistList->getArtists());
+    }
+
+    /**
+     * Test that we can add an instance of Artist
+     *
+     * @covers \MusicBrainz\Entity\ArtistList::addArtist
+     */
+    public function testAddArtist()
+    {
+        $artistList = new ArtistList();
+
+        $this->assertEmpty($artistList->getArtists());
+        $this->assertSame($artistList, $artistList->addArtist(new Artist()));
+        $this->assertCount(1, $artistList->getArtists());
+    }
+
+    /**
+     * Test that we can retrieve the artists property
+     *
+     * @covers \MusicBrainz\Entity\ArtistList::getArtists
+     */
+    public function testGetArtists()
+    {
+        $artistList = new ArtistList();
+
+        $this->assertInternalType('array', $artistList->getArtists());
+    }
+
+    /**
+     * Test that we can set the count property
+     *
+     * @covers \MusicBrainz\Entity\ArtistList::setCount
+     */
+    public function testGetSetCount()
+    {
+        $artistList = new ArtistList();
+        $count = 10;
+
+        $this->assertSame($artistList, $artistList->setCount($count));
+        $this->assertEquals($count, $artistList->getCount());
+    }
+
+    /**
+     * Test that we can set the offset property
+     *
+     * @covers \MusicBrain\Entity\ArtistList::setOffset
+     */
+    public function testGetSetOffset()
+    {
+        $artistList = new ArtistList();
+        $offset = 4;
+
+        $this->assertSame($artistList, $artistList->setOffset($offset));
+        $this->assertEquals($offset, $artistList->getOffset());
+    }
+
+    /**
+     * Test the iterator
+     *
+     * @covers \MusicBrainz\Entity\ArtistList::current
+     * @covers \MusicBrainz\Entity\ArtistList::key
+     * @covers \MusicBrainz\Entity\ArtistList::next
+     * @covers \MusicBrainz\Entity\ArtistList::rewind
+     * @covers \MusicBrainz\Entity\ArtistList::valid
+     */
+    public function testIterator()
+    {
+        $artists = array(
+            new Artist(), new Artist(), new Artist()
+        );
+        $artistList = new ArtistList();
+        $artistList->setArtists($artists);
+
+        $count = 0;
+        foreach ($artistList as $artist) {
+            $this->assertInstanceOf('\MusicBrainz\Entity\Artist', $artist);
+            $count++;
+        }
+
+        $this->assertCount($count, $artists);
+    }
+}
