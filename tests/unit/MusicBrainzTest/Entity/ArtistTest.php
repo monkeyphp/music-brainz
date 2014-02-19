5aec3520969dbf637758a2c29d0a232414a45328
David White <david@monkeyphp.com>
2014-02-19 22:07:52 +0000

    Added tests for Artist

--- tests/unit/MusicBrainzTest/Entity/ArtistTest.php
+++ tests/unit/MusicBrainzTest/Entity/ArtistTest.php
@@ -24,7 +24,10 @@
  */
 namespace MusicBrainzTest\Entity;
 
+use InvalidArgumentException;
+use MusicBrainz\Entity\Area;
 use MusicBrainz\Entity\Artist;
+use MusicBrainz\Entity\LifeSpan;
 use PHPUnit_Framework_TestCase;
 
 /**
@@ -103,88 +106,146 @@ class ArtistTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($name, $artist->getName());
     }
 
+    /**
+     * Test that we can get and set the sortName property
+     *
+     * @covers \MusicBrainz\Entity\Artist::getSortName
+     * @covers \MusicBrainz\Entity\Artist::setSortName
+     */
     public function testGetSetSortName()
     {
+        $artist = new Artist();
+        $sortName = 'Metallica';
 
+        $this->assertNull($artist->getSortName());
+        $this->assertSame($artist, $artist->setSortName($sortName));
+        $this->assertEquals($sortName, $artist->getSortName());
     }
 
-    public function testGetArea()
+    /**
+     * Test that we can get and set the Area
+     *
+     * @covers \MusicBrainz\Entity\Artist::getArea
+     * @covers \MusicBrainz\Entity\Artist::setArea
+     */
+    public function testGetSetArea()
+    {
+        $artist = new Artist();
+        $area = new Area();
+
+        $this->assertNull($artist->getArea());
+        $this->assertSame($artist, $artist->setArea($area));
+        $this->assertSame($area, $artist->getArea());
+    }
+
+    /**
+     * Test that we can get and set the beginArea
+     *
+     * @covers \MusicBrainz\Entity\Artist::getBeginArea
+     * @covers \MusicBrainz\Entity\Artist::setBeginArea
+     */
+    public function testGetSetBeginArea()
+    {
+        $artist = new Artist();
+        $area = new Area();
+
+        $this->assertNull($artist->getBeginArea());
+        $this->assertSame($artist, $artist->setBeginArea($area));
+        $this->assertSame($area, $artist->getBeginArea());
+    }
+
+    /**
+     * Test that we can get and set the LifeSpan
+     *
+     * @covers \MusicBrainz\Entity\Artist::getLifeSpan
+     * @covers \MusicBrainz\Entity\Artist::setLifeSpan
+     */
+    public function testGetSetLifeSpan()
+    {
+        $artist = new Artist();
+        $lifeSpan = new LifeSpan();
+
+        $this->assertNull($artist->getLifeSpan());
+        $this->assertSame($artist, $artist->setLifeSpan($lifeSpan));
+        $this->assertEquals($lifeSpan, $artist->getLifeSpan());
+    }
+
+    public function testGetSetAliasList()
     {
 
     }
 
-    public function testSetArea()
+    public function testGetSetIpiList()
     {
 
     }
 
-    public function testGetBeginArea()
+    public function testGetSetTagList()
     {
 
     }
 
-    public function testSetBeginArea()
-    {
-
-    }
-
-    public function testGetLifeSpan()
-    {
-
-    }
-
-    public function testSetLifeSpan()
-    {
-
-    }
-
-    public function testSetAliasList()
-    {
-
-    }
-
-    public function testGetAliasList()
-    {
-
-    }
-
-    public function testGetIpiList()
-    {
-
-    }
-
-    public function testSetIpiList()
-    {
-
-    }
-
-    public function testGetTagList()
-    {
-
-    }
-
-    public function testSetTagList()
-    {
-
-    }
-
+    /**
+     * Test that we can get and set the score
+     *
+     * @covers \MusicBrainz\Entity\Artist::getScore
+     * @covers \MusicBrainz\Entity\Artist::setScore
+     */
     public function testGetSetScore()
     {
+        $artist = new Artist();
+        $score = '100';
 
+        $this->assertNull($artist->getScore());
+        $this->assertSame($artist, $artist->setScore($score));
+        $this->assertEquals($artist->getScore(), $score);
     }
 
+    /**
+     * Test that we can get and set the gender
+     *
+     * @covers \MusicBrainz\Entity\Artist::getGender
+     * @covers \MusicBrainz\Entity\Artist::setGender
+     */
     public function testGetSetGender()
     {
+        $artist = new Artist();
+        $gender = 'male';
 
+        $this->assertNull($artist->getGender());
+        $this->assertSame($artist, $artist->setGender($gender));
+        $this->assertEquals($gender, $artist->getGender());
     }
 
+    /**
+     * Test that we can get and set the disambiguation
+     *
+     * @covers \MusicBrainz\Entity\Artist::getDisambiguation
+     * @covers \MusicBrainz\Entity\Artist::setDisambiguation
+     */
     public function testGetSetDisambiguation()
     {
+        $artist = new Artist();
+        $disambiguation = 'This is the disambiguation';
 
+        $this->assertNull($artist->getDisambiguation());
+        $this->assertSame($artist, $artist->setDisambiguation($disambiguation));
+        $this->assertEquals($disambiguation, $artist->getDisambiguation());
     }
 
+    /**
+     * Test that we can get and set the country
+     *
+     * @covers \MusicBrainz\Entity\Artist::getCountry
+     * @covers \MusicBrainz\Entity\Artist::setCountry
+     */
     public function testGetSetCountry()
     {
+        $artist = new Artist();
+        $country = 'US';
 
+        $this->assertNull($artist->getCountry());
+        $this->assertSame($artist, $artist->setCountry($country));
+        $this->assertEquals($country, $artist->getCountry());
     }
 }
