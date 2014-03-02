42dd0e284ba8979afe08e1157fe907066e42ad0d
David White <david@monkeyphp.com>
2014-03-02 17:21:03 +0000

    Added tests for Script

--- src/MusicBrainz/Entity/Script.php
+++ src/MusicBrainz/Entity/Script.php
@@ -2,6 +2,11 @@
 /**
  * Script.php
  *
+ * @category   MusicBrainz
+ * @package    MusicBrainz
+ * @subpackage MusicBrainz\Entity
+ * @author     David white <david@monkeyphp.com>
+ *
  * Copyright (C) David White <david@monkeyphp.com>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,23 +22,41 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 namespace MusicBrainz\Entity;
 
 /**
- * Description of Script
+ * Script
  *
- * @author David White <david@monkeyphp.com>
+ * @category   MusicBrainz
+ * @package    MusicBrainz
+ * @subpackage MusicBrainz\Entity
  */
 class Script
 {
+    /**
+     * The value of the Script
+     *
+     * @var string
+     */
     protected $script;
 
+    /**
+     * Constructor
+     *
+     * @param string $script
+     *
+     * @return void
+     */
     public function __construct($script)
     {
         $this->script = $script;
     }
 
+    /**
+     * Return a string representation of the Script
+     * 
+     * @return string
+     */
     public function __toString()
     {
         return $this->script;
